164afddb7d93713d14e273fe9f9ebac3e2e5ad81
浪子 <jkjoy@live.cn>
2024-10-12 11:05:17 +0800

    Update UploadImage.vue

--- front/components/UploadImage.vue
+++ front/components/UploadImage.vue
@@ -4,7 +4,7 @@
     <template #panel="{close}">
       <div class="p-4 flex flex-col gap-2">
         <div class="text-xs text-gray-400">本地上传</div>
-        <UInput type="file" size="sm" icon="i-heroicons-folder" @change="upload" multiple/>
+        <UInput type="file" size="sm" icon="i-heroicons-folder" @change="upload" multiple   accept="image/*" />
         <UTextarea :rows="5" placeholder="或者输入在线图片地址,逗号分隔,最多9张" v-model="imgs"/>
 
         <p v-if="filename" class="text-xs text-gray-400">正在上传({{ current }}/{{ total }})</p>
